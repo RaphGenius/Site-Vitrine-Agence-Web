ebdc2e5fe4adc456eaad0dde226d41b2105d6e6e
Raphael <guet.raphael@hotmail.com>
2022-10-27 12:08:08 +0200

    Page HOME fini avec effet + lien reseau sociaux & lien autres pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import DynamicText from "../components/DynamicText";
+import Mouse from "../components/Mouse";
 import Navigation from "../components/Navigation";
+import SocialNetwork from "../components/SocialNetwork";
 
 const Home = () => {
   return (
     <div>
-      <Navigation />
+      <Mouse />
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>AGENCY</h1>
+            <DynamicText />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
